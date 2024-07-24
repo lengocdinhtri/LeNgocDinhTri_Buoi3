7c3db9c97d4fd0a38767cc73d9084331d66b73c5
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-07-24 15:37:05 +0700

    Bai1_Buoi3_c1

---
+++ Bai1_Buoi3/Bai1Buoi3.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 100
+
+void TaoMaTran(int a[MAX][MAX], int m, int n, int k) {
+	srand(time(NULL));
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = rand() % (k + 1);
+		}
+	}
+}
+
+void XuatMaTran(int a[MAX][MAX], int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%d\t", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+
+int main() {
+	int m, n, k;
+	int a[MAX][MAX];
+
+	printf("Nhap so dong: ");
+	scanf("%d", &m);
+	printf("Nhap so cot: ");
+	scanf("%d", &n);
+	printf("Nhap gia tri k de tao cac so ngau nhien tu 0 den k: ");
+	scanf("%d", &k);
+
+	TaoMaTran(a, m, n, k);
+
+	printf("Ma tran ngau nhien:\n");
+	XuatMaTran(a, m, n);
+
+	return 0;
+}
