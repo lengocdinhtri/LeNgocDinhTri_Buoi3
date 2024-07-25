f35cc06914be4ca32bbfa38328c55f1c228976c0
Le Ngoc Dinh Tri <dinhtri5112003@gmail.com>
2024-07-25 21:27:03 +0700

    Bai2_Buoi3_c1_Nhap xuat ma tran vuong chua so nguyen ngau nhien dk n>=5

---
+++ Bai2_Buoi3/Bai2_Buoi3/Bai2_Buoi3.cpp
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void TaoMaTran(int n, int **a) {
+	srand(time(NULL));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = rand() % 100; // Giá trị ngẫu nhiên từ 0 đến 99
+		}
+	}
+}
+
+void XuatMaTran(int n, int **a) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%3d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+
+int main() {
+	int n;
+	printf("Nhap cap cua ma tran (n >= 5): ");
+	scanf("%d", &n);
+
+	if (n < 5) {
+		printf("Cap cua ma tran phai lon hon hoac bang 5.\n");
+		return 1;
+	}
+
+	int **a = (int **)malloc(n * sizeof(int *));
+	for (int i = 0; i < n; i++) {
+		a[i] = (int *)malloc(n * sizeof(int));
+	}
+
+	TaoMaTran(n, a);
+
+	printf("Ma tran ngau nhien:\n");
+	XuatMaTran(n, a);
+
+	return 0;
+}
